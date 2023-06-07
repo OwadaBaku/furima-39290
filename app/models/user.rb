ff0291acbb358e62e5fc4bbfb57fc8429ec7c9c8
OwadaBaku <owd.iphn7@gmail.com>
2023-06-07 15:13:59 +0900

    バリデーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ validates :nickname, presence: true
 validates :birthday, presence: true
 
   with_options presence: true do
-    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i , message: "is invalid. Include both letters and numbers"}
+    validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i , message: "is invalid. Include both letters and numbers"}
     validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     validates :last_name_kana, format: {with: /\A[ァ-ヶー]+\z/, message: "is invalid. Input full-width katakana characters."}
