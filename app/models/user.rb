1e08b95f996f24321bb1008acbdc1282ce2c15da
OwadaBaku <owd.iphn7@gmail.com>
2023-06-08 12:48:49 +0900

    バリデーションの修正3

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,7 @@ class User < ApplicationRecord
   with_options presence: true do
     validates :nickname
     validates :birthday
+    validates :encrypted_password
     validates :password, format: {with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i , message: "is invalid. Include both letters and numbers"}
     validates :last_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
     validates :first_name, format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: "is invalid. Input full-width characters."}
