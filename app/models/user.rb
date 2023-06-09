e18d6c07a220fabb04210698e34e76e5b297b4a3
OwadaBaku <owd.iphn7@gmail.com>
2023-06-09 18:16:02 +0900

    Rubocop自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :password,
-  format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
+            format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
 
   with_options presence: true do
     validates :nickname
