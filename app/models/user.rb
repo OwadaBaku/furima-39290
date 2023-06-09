a3fd30aea18a77028f17eb3fb77fa29cc9dc3c5c
OwadaBaku <owd.iphn7@gmail.com>
2023-06-09 12:02:43 +0900

    記述は不要なため削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,16 +2,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  def new
-    @user = User.new
-  end
-
   with_options presence: true do
     validates :nickname
     validates :birthday
-    validates :encrypted_password
+    validates :password_confirmation
     validates :password,
-              format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
+              format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
     validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
     validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'is invalid. Input full-width characters.' }
     validates :last_name_kana, format: { with: /\A[ァ-ヶー]+\z/, message: 'is invalid. Input full-width katakana characters.' }
