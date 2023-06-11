d7ae06bab30c62c04d58dd4e114c77058a5e72cd
OwadaBaku <owd.iphn7@gmail.com>
2023-06-11 13:39:37 +0900

    出品機能実装のためコントローラーを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,9 +5,22 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
-  def message_params
-    params.require(:item).permit(:content, :image).merge(user_id: current_user.id)
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: "出品が完了しました。"
+    else
+      render :new
+    end
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :item_name, :item_description, :category_id, :item_condition_id, :item_postage_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+  end
+
 end
