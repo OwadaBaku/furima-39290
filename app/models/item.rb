e91f6be1b7c85b1ea9e754b1442afc0e6ce741cd
OwadaBaku <owd.iphn7@gmail.com>
2023-06-11 13:37:58 +0900

    itemsテーブルにurse_idを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
   belongs_to :category
   belongs_to :item_condition
   belongs_to :item_postage
@@ -7,9 +8,10 @@ class Item < ApplicationRecord
   belongs_to :shipping_day
 	has_one_attached :image
 
-	# # 空の投稿を保存できないようにする
-  # validates :item_name, :item_description, :price, presence: true
 
-  # # ジャンルの選択が「---」の時は保存できないようにする
-  # validates :category_id, :item_condition_id, :item_postage_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
+  #空の投稿を保存できないようにする
+  validates :item_name, :item_description, :price, presence: true
+
+  #ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, :item_condition_id, :item_postage_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1 } 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
-  resources :items, only: [:index, :new]
+  resources :items, only: [:index, :new, :create]
+  post '/items/new', to: 'items#create'
 end
