f893d173ed041da163d790936ad3ba62e423b718
OwadaBaku <owd.iphn7@gmail.com>
2023-06-12 12:59:56 +0900

    Rubocopで自動修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path, notice: "出品が完了しました。"
+      redirect_to root_path, notice: '出品が完了しました。'
     else
       render :new
     end
@@ -20,7 +20,7 @@ class ItemsController < ApplicationController
   private
 
   def item_params
-    params.require(:item).permit(:image, :item_name, :item_description, :category_id, :item_condition_id, :item_postage_id, :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :item_name, :item_description, :category_id, :item_condition_id, :item_postage_id,
+                                 :prefecture_id, :shipping_day_id, :price).merge(user_id: current_user.id)
   end
-
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -7,15 +7,15 @@ class Item < ApplicationRecord
   belongs_to :item_postage
   belongs_to :prefecture
   belongs_to :shipping_day
-	has_one_attached :image
+  has_one_attached :image
 
-  #空の投稿を保存できないようにする
+  # 空の投稿を保存できないようにする
   validates :item_name, :item_description, :price, presence: true
-  #商品画像を1枚つけないと保存できないようにする
+  # 商品画像を1枚つけないと保存できないようにする
   validates :image, presence: true
   # 価格は、¥300~¥9,999,999の間以外は保存できないようにする
   validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   validates :price, format: { with: /\A[0-9]+\z/, message: 'Price is invalid. Input half-width characters.' }
-  #ジャンルの選択が「---」の時は保存できないようにする
-  validates :category_id, :item_condition_id, :item_postage_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1 } 
+  # ジャンルの選択が「---」の時は保存できないようにする
+  validates :category_id, :item_condition_id, :item_postage_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1 }
 end
