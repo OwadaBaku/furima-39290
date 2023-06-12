7fe4e911db2ca7b4a3de89c62fceab4c05e58924
OwadaBaku <owd.iphn7@gmail.com>
2023-06-12 15:31:43 +0900

    半角整数値のみ許容するバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,8 +14,10 @@ class Item < ApplicationRecord
   # 商品画像を1枚つけないと保存できないようにする
   validates :image, presence: true
   # 価格は、¥300~¥9,999,999の間以外は保存できないようにする
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
-  validates :price, format: { with: /\A[0-9]+\z/, message: 'Price is invalid. Input half-width characters.' }
+  # 価格は、半角数字以外は保存できないようにする
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 },
+            format: { with: /\A[0-9]+\z/ }
+
   # ジャンルの選択が「---」の時は保存できないようにする
   validates :category_id, :item_condition_id, :item_postage_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1 }
 end
