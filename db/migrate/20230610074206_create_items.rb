767e92ea0bdf8846e4ebd9829f67c68af4f4dbd7
OwadaBaku <owd.iphn7@gmail.com>
2023-06-13 15:03:09 +0900

    itemテーブル修正

--- db/migrate/20230610074206_create_items.rb
+++ db/migrate/20230610074206_create_items.rb
@@ -1,7 +1,7 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      t.integer :user_id,           null: false, foreign_key: true
+      t.references :user,         null: false, foreign_key: true
       t.string  :item_name,         null: false
       t.text    :item_description,  null: false
       t.integer :category_id,       null: false
