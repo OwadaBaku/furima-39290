9e952ad7c38f517ac08c6f8c14650e1459cd50ed
OwadaBaku <owd.iphn7@gmail.com>
2023-06-13 15:03:21 +0900

    Rubcop実行

--- db/schema.rb
+++ db/schema.rb
@@ -34,7 +34,7 @@ ActiveRecord::Schema.define(version: 2023_06_11_010026) do
   end
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "user_id", null: false
+    t.bigint "user_id", null: false
     t.string "item_name", null: false
     t.text "item_description", null: false
     t.integer "category_id", null: false
@@ -45,15 +45,7 @@ ActiveRecord::Schema.define(version: 2023_06_11_010026) do
     t.integer "price", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "orders", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "item_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["item_id"], name: "index_orders_on_item_id"
-    t.index ["user_id"], name: "index_orders_on_user_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -75,6 +67,5 @@ ActiveRecord::Schema.define(version: 2023_06_11_010026) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "orders", "items"
-  add_foreign_key "orders", "users"
+  add_foreign_key "items", "users"
 end
