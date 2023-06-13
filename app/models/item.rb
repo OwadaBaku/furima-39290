2fea356a1e5c26c851528b87247a5c3ea6cf1754
OwadaBaku <owd.iphn7@gmail.com>
2023-06-13 11:42:32 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -15,9 +15,7 @@ class Item < ApplicationRecord
   validates :image, presence: true
   # 価格は、半角数字で¥300~¥9,999,999の間以外は保存できないようにする
   # 価格は、数字保存できないようにする
-  validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                    format: { with: /\A[0-9]+\z/ }
-
+  validates :price, numericality: { only_integer: true }, inclusion: { in: 300..9_999_999 }
   # ジャンルの選択が「---」の時は保存できないようにする
   validates :category_id, :item_condition_id, :item_postage_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1 }
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -93,12 +93,12 @@ RSpec.describe Item, type: :model do
       it 'priceが¥300より小さいと出品できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+        expect(@item.errors.full_messages).to include('Price is not included in the list')
       end
       it 'priceが¥9,999,999より大きいと出品できない' do
         @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
+        expect(@item.errors.full_messages).to include('Price is not included in the list')
       end
       it 'priceが半角数値以外の場合出品できない' do
         # priceが全角数字の場合
