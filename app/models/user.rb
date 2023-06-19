94a194fe4424ea5ede3818ba179982b0ed33d96e
OwadaBaku <owd.iphn7@gmail.com>
2023-06-19 17:22:20 +0900

    モデルアソシエーション修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :order
+  has_many :items
+  has_many :orders
 
   validates :password,
             format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: 'is invalid. Include both letters and numbers' }
