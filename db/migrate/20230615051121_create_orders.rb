6d97738fa55059ed4f2fa7645c03cd80095e0ec2
OwadaBaku <owd.iphn7@gmail.com>
2023-06-19 12:40:34 +0900

    マイグレーション

--- db/migrate/20230615051121_create_orders.rb
+++ db/migrate/20230615051121_create_orders.rb
@@ -1,8 +1,8 @@
 class CreateOrders < ActiveRecord::Migration[6.0]
   def change
     create_table :orders do |t|
-      t.references :user,         null: false, foreign_key: true
-      t.references :item,         null: false, foreign_key: true
+      t.bigint :user_id, null: false
+      t.bigint :item_id, null: false
       t.timestamps
     end
   end
